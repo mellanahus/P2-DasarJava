b2aeb8eda3e30252d8452fbfca426b05e62e4d37
mellanahus <Mellanahus02SJ@gmail.com>
2022-03-17 21:17:44 +0700

    Percobaan 3 melakukan increment dan decrement nilai

--- src/C_IncDec.java
+++ src/C_IncDec.java
@@ -3,12 +3,12 @@
 public class C_IncDec {
     public static void main(String[] args) {
 
-        int x = 8, y =13;
-        System.out.println("x = "+x);
-        System.out.println("y = "+y);
-        System.out.println("x = "+ ++x);
-        System.out.println("y = "+ y++);
-        System.out.println("x = "+ x--);
-        System.out.println("y = "+ --y);
+        int x = 8, y = 13;
+        System.out.println("x= "+x);
+        System.out.println("y= "+y);
+        System.out.println("x= "+ ++x);
+        System.out.println("y= "+ y++);
+        System.out.println("x= "+ x--);
+        System.out.println("y= "+ --y);
     }
 }
