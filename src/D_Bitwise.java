dea0950f7a09742711fc6298fbbaef4766591d8b
mellanahus <Mellanahus02SJ@gmail.com>
2022-03-17 21:36:00 +0700

    Percobaan 4 melakukan operasi bit

---
+++ src/D_Bitwise.java
@@ -0,0 +1,13 @@
+// percobaan melakukan operasi bit
+
+public class D_Bitwise {
+    public static void main(String[] args) {
+
+        int x=5, y=6;
+        System.out.println("x= "+x);
+        System.out.println("y= "+y);
+        System.out.println("x&y = "+(x&y));
+        System.out.println("x|y= "+(x|y));
+        System.out.println("x^y= "+(x^y));
+    }
+}
