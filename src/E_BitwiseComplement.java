c7106a84b030826583471d6887f02d8fdac4b650
mellanahus <Mellanahus02SJ@gmail.com>
2022-03-17 21:41:37 +0700

    Percobaan 5 melakukan operasi komplemen

---
+++ src/E_BitwiseComplement.java
@@ -0,0 +1,11 @@
+// percobaan melakukan operasi komplemen
+
+public class E_BitwiseComplement {
+    public static void main(String[] args) {
+
+        int x=8;
+        System.out.println("x= "+x);
+        int y= ~x;
+        System.out.println("y= "+y);
+    }
+}
