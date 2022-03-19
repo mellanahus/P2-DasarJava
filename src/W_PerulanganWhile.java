7dd4e5ba2a0ac8f1a89d0abe71ca53cf67d3163e
mellanahus <Mellanahus02SJ@gmail.com>
2022-03-19 16:45:53 +0700

    Percobaan 23 menggunakan perulangan for each

---
+++ src/W_PerulanganWhile.java
@@ -0,0 +1,22 @@
+// percobaan menggunakan perulangan for each
+
+public class W_PerulanganWhile {
+    public static void main(String[] args) {
+
+        //while loop
+        int x=0;
+        System.out.println("Example of While Loop-- ");
+        while (x<=10) {
+            System.out.printf("%d ",x);
+            x++;
+        }
+        System.out.print('\n');
+
+        //do-while loop
+        int y=0;
+        do {
+            y=y+10;
+            System.out.println("I am inside Do block--- "+y);
+        } while (y<=50);
+    }
+}
