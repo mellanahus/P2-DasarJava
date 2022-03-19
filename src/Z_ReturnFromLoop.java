73a4146740bc558f488dfab6145260256a86f2db
mellanahus <Mellanahus02SJ@gmail.com>
2022-03-19 16:52:15 +0700

    Percobaan 26 menggunakan return pada perulangan for

---
+++ src/Z_ReturnFromLoop.java
@@ -0,0 +1,22 @@
+// percobaan menggunakan return pada perulangan for
+
+public class Z_ReturnFromLoop {
+    public static void main(String[] args) {
+
+        returnLoop();
+    }
+    static void returnLoop() {
+        int x;
+        System.out.println("Example of return in For-Loop");
+        for(x=1; x<=10; x++) {
+            if(x<5) {
+                System.out.printf("%d ",(x-1));
+            } else {
+                System.out.print("\n");
+                return;
+            }
+            System.out.printf("\n");
+        }
+        System.out.print("Tidak akan pernah di eksekusi");
+    }
+}
