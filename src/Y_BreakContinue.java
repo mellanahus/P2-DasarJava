ba6548549ecf1a1b37204bad5bc335e033283482
mellanahus <Mellanahus02SJ@gmail.com>
2022-03-19 16:50:41 +0700

    Percobaan 25 penggunaan break dan continue pada perulangan for

---
+++ src/Y_BreakContinue.java
@@ -0,0 +1,19 @@
+// percobaan menggunakan break dan continue pada perulangan for
+
+public class Y_BreakContinue {
+    public static void main(String[] args) {
+
+        int x;
+        System.out.println("Example of Break and Continue in For-Loop");
+        for(x=1; x<=10; x++) {
+            if (x == 7) {
+                break;
+            }
+            if (x == 3) {
+                continue;
+            }
+            System.out.printf("%d ", x);
+        }
+        System.out.print('\n');
+        }
+    }
