27afb5df20bbaa85704a44f2c0a5e7fc89f53173
mellanahus <Mellanahus02SJ@gmail.com>
2022-03-19 15:50:00 +0700

    Percobaan 20 menggunakan perulangan for each

---
+++ src/T_PerulanganForEach.java
@@ -0,0 +1,17 @@
+// percobaan menggunakan perulangan for each
+
+public class T_PerulanganForEach {
+    public static void main(String[] args) {
+
+        int arrAngka[] = {10, 11, 12, 13, 14, 15};
+        for(int angka:arrAngka) {
+            System.out.printf("%d ",angka);
+        }
+        System.out.print("\n");
+
+        String[]cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for(String car:cars) {
+            System.out.println(car);
+        }
+    }
+}
