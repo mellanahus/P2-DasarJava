cc48b6376e65ae85d65535b87d6e46060448bcc9
mellanahus <Mellanahus02SJ@gmail.com>
2022-03-19 13:24:23 +0700

    Percobaan 12 menggunakan operator kondisi

---
+++ src/L_Conditional.java
@@ -0,0 +1,12 @@
+// percobaan menggunakan operasi kondisi
+
+public class L_Conditional {
+    public static void main(String[] args) {
+
+        int x=0;
+        boolean isEven=false;
+        System.out.println("x= "+x);
+        x= isEven ?4:7;
+        System.out.println("x= "+x);
+    }
+}
